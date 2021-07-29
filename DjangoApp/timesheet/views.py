26a6d8d9c43d1a8d1ef00d5ab2b0a073a42343fa
Clayton Cook <claytoncook12@gmail.com>
2021-07-29 12:59:42 -0400

    add requirement to sign in to home page

--- DjangoApp/timesheet/views.py
+++ DjangoApp/timesheet/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from timesheet.forms import SignInEntryForm, SignOutEntryForm
 
 # Create your views here.
 def timesheet_home(request):
-    return render(request, 'timesheet/home.html')
+    if request.user.is_anonymous:
+        return redirect('account_login')
+    else:
+        return render(request, 'timesheet/home.html')
 
 def timesheet_signin(request):
     if request.method == 'POST':
